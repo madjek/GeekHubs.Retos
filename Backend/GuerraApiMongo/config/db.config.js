494f5c9d172e20bf24ef02e5205a24336dd16bfb
madjek <90720831+madjek@users.noreply.github.com>
2021-11-09 10:20:07 +0100

    config + index updated

--- Backend/GuerraApiMongo/config/db.config.js
+++ Backend/GuerraApiMongo/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb://user:password@100.0.0.0:27017/guerra_db?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false" //Cadena de conexión mongo db
+    url: "mongodb://Eugene:1Jekyna2@192.168.1.138:27017/guerra_db?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false" //Cadena de conexión mongo db
   };
